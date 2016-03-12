83e0827783a4f04b8f54ddf971c33599e598fcd9
xwibxticx <xwibxticx@gmail.com>
2016-03-13 00:21:51 +0100

    Recuperation token authentification

--- hackchtx01-lpw.py
+++ hackchtx01-lpw.py
@@ -1,6 +1,9 @@
 from flask import Flask, request, session, g, redirect, url_for, \
      abort, render_template, flash
 import requests
+import urllib.parse as urllib
+import json
+import pickle
 
 # configuration
 #export FLASKR_SETTINGS = '/root/Bureau/hackchtx01-lpw/configuration.cfg'
@@ -13,15 +16,22 @@ app = Flask(__name__)
 app.config.from_envvar('FLASKR_SETTINGS')
 
 
+class User(object):
+
+    def __init__(self, token):
+        self.name = "Matthieu"
+        self.token = token
+
+    def __str__(self):
+        return self.name+" et "+self.token
+
+
 @app.route('/')
 def show_entries():
-    r = requests.get("http://www.instructables.com")
-
-    r.text
-    status=r.status_code,
-    content_type=r.headers['content-type'],
-
-    entries = [{"title": "titre1", "text": r.text}, {"title": "titre2", "text": r.text}]
+    entries=[
+        {"title": "titre1", "text": 'a'},
+        {"title": "titre2", "text": 'a'}
+    ]
     return render_template('show_entries.html', entries=entries)
 
 
@@ -33,10 +43,26 @@ def add_entry():
     return redirect(url_for('show_entries'))
 
 
+@app.route('/session', methods=['GET'])
+def display_session():
+    return "<body>"+session['token']+"<br>" \
+               +str(session['user'])+"<br>" \
+               +str(pickle.loads(session['user']))+"</body>"
+
+
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     error = None
     if request.method == 'POST':
+        url = 'http://localhost:8080/psdp/rest/auth/token'
+        payload = {'grant_type': 'password', 'client_id': 'd1f53029-9732-4d00-ba8e-0304020bef6b', 'client_secret': 'secret', 'username': 'demo@demo.com', 'password': 'demo'}
+        headers = {'content-type': 'application/x-www-form-urlencoded', 'Accept': 'application/json'}
+        r = requests.post(url, data=urllib.urlencode(payload), headers=headers)
+        print(r.status_code)
+        session['token'] = json.loads(r.text)['access_token']
+        user = User(session['token'])
+        session['user'] = pickle.dumps(user)
+
         if request.form['username'] != app.config['USERNAME']:
             error = 'Invalid username'
         elif request.form['password'] != app.config['PASSWORD']:
